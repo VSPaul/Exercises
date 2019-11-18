ae8b0b1691350be51ff27d62519105f36280df55
VSPaul <vascupaul_vsp@yahoo.com>
2019-11-18 15:55:57 +0200

    Updates

---
+++ precache-manifest.ce724f431a40fe89028608e858b9f112.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2855cf69d54f77168e246df4592b454b",
+    "url": "/my-travels/index.html"
+  },
+  {
+    "revision": "454531f38b7cd347ed01",
+    "url": "/my-travels/static/css/main.1e26c8bb.chunk.css"
+  },
+  {
+    "revision": "a25de4ead129486927ea",
+    "url": "/my-travels/static/js/2.af63ec1a.chunk.js"
+  },
+  {
+    "revision": "454531f38b7cd347ed01",
+    "url": "/my-travels/static/js/main.59173123.chunk.js"
+  },
+  {
+    "revision": "a5dc84c49447d90a4c28",
+    "url": "/my-travels/static/js/runtime-main.443348f8.js"
+  }
+]);
